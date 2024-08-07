2156a080ac460cd466770c8f64a7681724db9c8b
ayan_sk <ayan.sk@gmail.com>
2024-08-07 11:33:27 +0000

    updated notice

--- lib/notice.dart
+++ lib/notice.dart
@@ -2,28 +2,51 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:http/http.dart' as http;
 
-class NoticeScreen extends StatelessWidget {
+class NoticeScreen extends StatefulWidget {
+  @override
+  _NoticeScreenState createState() => _NoticeScreenState();
+}
+
+class _NoticeScreenState extends State<NoticeScreen> {
+  String _htmlContent = '<html><body><h1>Loading...</h1></body></html>';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchNotice();
+  }
+
+  Future<void> _fetchNotice() async {
+    final response = await http.post(
+      Uri.parse('https://rohinicomplex.in/service/getNotice.php'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({}), // Add any necessary parameters here
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        // Decode the base64 encoded response body
+        _htmlContent = utf8.decode(base64Decode(response.body));
+      });
+    } else {
+      setState(() {
+        _htmlContent =
+            '<html><body><h1>Error loading notice</h1></body></html>';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HTML Viewer'),
+        title: Text('Notice Board'),
       ),
       body: WebView(
         initialUrl: Uri.dataFromString(
-          """
-            <!DOCTYPE html>
-            <html>
-            <head>
-              <title>Test HTML</title>
-            </head>
-            <body>
-              <h1>Hello, World!</h1>
-              <p>This is a paragraph in HTML.</p>
-            </body>
-            </html>
-          """,
+          _htmlContent,
           mimeType: 'text/html',
           encoding: Encoding.getByName('utf-8'),
         ).toString(),
